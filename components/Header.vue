c0400c2480365929177e1a3bb7a650b978ae277e
Iam4coaching <evenementen@iam4coaching.nl>
2025-04-16 12:46:47 +0200

    header

--- components/Header.vue
+++ components/Header.vue
@@ -1,10 +1,13 @@
 <template>
-    <header class="2xl:container 2xl:mx-auto px-8 py-12 bg-[#3E7B08] flex flex-row-reverse">
+    <header class="h-100 bg-[#3E7B08] flex justify-between">
+        <div>
 
-            <button class="font-bold  text-lg  m-20 w-64 h-24 bg-[#3E7B08] text-[#ffffff] justify-center ">About</button>
-            <button class="font-bold  text-lg  m-20 w-64 h-24 bg-[#3E7B08] text-[#ffffff] justify-center ">Pricing</button>
-            <button class="font-bold  text-lg  m-20 w-64 h-24 bg-[#3E7B08] text-[#ffffff] justify-center ">Contact</button>
-            <button class="font-bold  text-lg  m-20 w-64 h-24 bg-[#3E7B08] text-[#ffffff] justify-center">Sign up</button>
-
+        </div>
+    <div class=" flex flex-row-row">
+            <button class="font-bold text-lg h-full px-12 py-12 bg-[#3E7B08] hover:bg-[#58AA0E] text-[#ffffff] justify-center ">About</button>
+            <button class="font-bold text-lg h-full px-12 py-12 bg-[#3E7B08] hover:bg-[#58AA0E] text-[#ffffff] justify-center ">Pricing</button>
+            <button class="font-bold text-lg h-full px-12 py-12 bg-[#3E7B08] hover:bg-[#58AA0E] text-[#ffffff] justify-center ">Contact</button>
+            <button class="font-bold text-lg h-full mr-20 px-12 py-12 bg-[#3E7B08] hover:bg-[#58AA0E] text-[#ffffff] justify-center">Sign up</button>
+    </div>
     </header>
 </template>
