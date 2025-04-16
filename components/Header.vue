eca41a73f2e2a443b64ca6e23d205861475fe7fa
Iam4coaching <evenementen@iam4coaching.nl>
2025-04-16 11:20:23 +0200

    Layout added

---
+++ components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <header class="2xl:container 2xl:mx-auto px-8 py-12 bg-[#3E7B08] flex flex-row-reverse">
+
+            <button class="font-bold  text-lg  m-20 w-64 h-24 bg-[#3E7B08] text-[#ffffff] justify-center ">About</button>
+            <button class="font-bold  text-lg  m-20 w-64 h-24 bg-[#3E7B08] text-[#ffffff] justify-center ">Pricing</button>
+            <button class="font-bold  text-lg  m-20 w-64 h-24 bg-[#3E7B08] text-[#ffffff] justify-center ">Contact</button>
+            <button class="font-bold  text-lg  m-20 w-64 h-24 bg-[#3E7B08] text-[#ffffff] justify-center">Sign up</button>
+
+    </header>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <Header/>
+      <p>Some default layout content shared across all pages</p>
+      <slot />
+      <div class="fixed inset-0 bg-gradient-to-tr from-white to-green-100 z-[-1] " />
+    </div>
+  </template>
