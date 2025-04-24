c83b4cafd67e43b40321fa63020f4d9b2749571e
Iam4coaching <evenementen@iam4coaching.nl>
2025-04-24 12:04:34 +0200

    im crying from foreachs

---
+++ components/GalleryImage.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { defineProps } from 'vue';
+
+    const props = defineProps({
+        id: Number,
+        alt: String,
+    })
+</script>
+<template>
+    <div id="{{ id }}">
+        <p>{{ props.id }}</p>
+        <p>{{ props.alt }}</p>
+    </div>
+</template>
--- pages/gallery.vue
+++ pages/gallery.vue
@@ -1,19 +1,21 @@
 <script lang="ts">
+import { ref } from 'vue'
 
-
-const photos = [
-  'https://picsum.photos/640/640?random=1',
-  'https://picsum.photos/640/640?random=2',
-  'https://picsum.photos/640/640?random=3',
-  'https://picsum.photos/640/640?random=4',
-  'https://picsum.photos/640/640?random=5',
-  'https://picsum.photos/640/640?random=6'
+  const photos = [
+  {url:"https://picsum.photos/640/640?random=1",alt:"photo 1"},
+  {url:"https://picsum.photos/640/640?random=2",alt:"photo 2"},
+  {url:"https://picsum.photos/640/640?random=3",alt:"photo 3"},
+  {url:"https://picsum.photos/640/640?random=4",alt:"photo 4"},
+  {url:"https://picsum.photos/640/640?random=5",alt:"photo 5"},
+  {url:"https://picsum.photos/640/640?random=6",alt:"photo 6"},
 ]
-
+console.log(photos)
 </script>
 
 <template>
       <div>
-        <img class="h-auto max-w-full rounded-lg" src="{image}" alt="">
+        <li>
+          {{ photos[1].alt }}
+        </li>
     </div>
 </template>
