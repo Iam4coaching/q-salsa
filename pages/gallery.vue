aef6a18581bb12ba6a046e050514455e5919fc5d
Iam4coaching <evenementen@iam4coaching.nl>
2025-04-25 10:00:12 +0200

    gallery
    gallery set up

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -1,21 +1,24 @@
-<script lang="ts">
+<script setup lang="ts">
 import { ref } from 'vue'
 
-  const photos = [
-  {url:"https://picsum.photos/640/640?random=1",alt:"photo 1"},
-  {url:"https://picsum.photos/640/640?random=2",alt:"photo 2"},
-  {url:"https://picsum.photos/640/640?random=3",alt:"photo 3"},
-  {url:"https://picsum.photos/640/640?random=4",alt:"photo 4"},
-  {url:"https://picsum.photos/640/640?random=5",alt:"photo 5"},
-  {url:"https://picsum.photos/640/640?random=6",alt:"photo 6"},
-]
-console.log(photos)
+const photos = ref([
+  { url: "https://picsum.photos/640/640?random=1", alt: "photo 1" },
+  { url: "https://picsum.photos/640/640?random=2", alt: "photo 2" },
+  { url: "https://picsum.photos/640/640?random=3", alt: "photo 3" },
+  { url: "https://picsum.photos/640/640?random=4", alt: "photo 4" },
+  { url: "https://picsum.photos/640/640?random=5", alt: "photo 5" },
+  { url: "https://picsum.photos/640/640?random=6", alt: "photo 6" },
+])
 </script>
 
 <template>
-      <div>
-        <li>
-          {{ photos[1].alt }}
-        </li>
+  <div class="grid grid-cols-2 md:grid-cols-3 gap-10 content-normal p-10">
+    <div v-for="photo in photos" :key="photo.url" class="m-10">
+      <img
+        class="h-400 w-400 rounded-lg"
+        :src="photo.url"
+        :alt="photo.alt"
+      />
     </div>
+  </div>
 </template>
