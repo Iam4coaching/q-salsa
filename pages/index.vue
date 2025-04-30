2285b0753af8158348a949f90df73fad20814e26
Iam4coaching <evenementen@iam4coaching.nl>
2025-04-30 09:36:47 +0200

    fixed image error

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,10 @@ definePageMeta({
     <Carousel/>
         
     <div class="flex flex-row justify-between h-150 background-animate bg-gradient-to-r from-red-300  via-green-300 to-purple-500 inset-x-0 sticky bottom-0 ">
-         <Img src="/Images/Qsalsa_Logo.png" height="600" width="600" class="absolute -top-150 ml-12"/>   
+         <div class="absolute -top-150 ml-12">
+          <Img src="/Images/Qsalsa_Logo.png" height="600" width="600"/>
+         </div>
+         
          <button class="font-bold mr-30 px-12 py-12 text-6xl h-full text-[#ffffff] absolute inset-y-0 right-0">Check it out</button>
     </div>
     </div>
